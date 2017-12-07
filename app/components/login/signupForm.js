17b43be5268e73a87b7c37fbc467cda8cec94216
Porfirio Matias <SSJ6Porfy@hotmail.com>
2017-12-07 12:39:10 -0500

    created signup form & container

---
+++ app/components/login/signupForm.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { StyleSheet, 
+    Text, 
+    View, 
+    Dimensions, 
+    TextInput,
+    ScrollView,
+    Button,
+    TouchableOpacity,
+    } from 'react-native';
+
+class SignupForm extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <Text>I'm the SignUp Page</Text>
+        );
+    }
+}
+
+export default SignupForm;
